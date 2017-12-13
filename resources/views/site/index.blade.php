067b7534ecb3586d65c9abf6558e2291ac9181d5
Futura One <futuraone@icloud.com>
2017-12-13 14:56:27 -0200

    banners

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -9,7 +9,7 @@
 
 @section('main')
 
-	<div id="carousel-example-generic" class="carousel slide animated fadeInDown" data-ride="carousel">
+	<div id="carousel-main" class="carousel slide animated fadeInDown" data-ride="carousel">
 		<ol class="carousel-indicators">
 			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
 			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
@@ -26,11 +26,11 @@
 				<img src="{{ asset('img/home/banners/ProductBanner.jpg') }}" alt="...">
 			</div>
 		</div>
-		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		<a class="left carousel-control" href="#carousel-main" role="button" data-slide="prev">
 			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 			<span class="sr-only">Previous</span>
 		</a>
-		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		<a class="right carousel-control" href="#carousel-main" role="button" data-slide="next">
 			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 			<span class="sr-only">Next</span>
 		</a>
