5242536d032a284866da6818b79d10bcf69364ee
Futura One <futuraone@icloud.com>
2017-12-13 16:39:58 -0200

    teste

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -23,8 +23,8 @@ class IndexController extends Controller
 	{
 //		$treinoDoDia = $this->trainingModel->treinoDoDia();
 //		$outrosTreinos = $this->trainingModel->skip(1)->take(3)->get();
-		$goals = $this->goalModel->tudo();
+//		$goals = $this->goalModel->tudo();
 
-		return view('site.index', compact('goals'));
+		return view('site.index');
 	}
 }
