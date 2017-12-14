3383ec18e9d7dcd8298282b4323952e96e3e095e
Futura One <futuraone@icloud.com>
2017-12-14 08:06:39 -0200

    js

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,8 +1,8 @@
-@extends('.site.main')
-@section('content')
+@extends('site.main')
+@section('main')
 
-    <div class="content" style="width: 50%;position:absolute;top:35%;left:25%;">
-        <div class="row">
+    <section id="main">
+        <div class="container">
             <div class="col-md-12">
                 <div style="font-size: 18px; margin-bottom: 40px;" class="alert alert-danger text-center">Error HTTP 500 Internal Server Error</div>
 
@@ -12,10 +12,10 @@
 
                 <br />
                 <p style="text-align: center;">
-                    <a class="btn btn-lg btn-warning" href="javascript:window.history.back();">Click here to return to the start page</a>
+                    <a class="btn btn-lg btn-warning" href="javascript:window.history.back();">Clique para Retornar a Página Anterior</a>
                 </p>
             </div>
         </div>
-    </div>
+    </section>
 
 @endsection
