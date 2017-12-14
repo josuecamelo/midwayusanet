fc0085936b3b1e4b018f3068d0013a1b7a9deaa3
Futura One <futuraone@icloud.com>
2017-12-14 11:04:43 -0200

    Carousel

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -2,16 +2,65 @@
 
 @section('css')
 	<style>
-.meu_estilo{
-	background: red;
-}
+
+		/* Carousel: */
+
+		.carousel-fade .carousel-inner .item {
+			-webkit-transition-property: opacity;
+			transition-property: opacity;
+		}
+
+		.carousel-fade .carousel-inner .item,
+		.carousel-fade .carousel-inner .active.left,
+		.carousel-fade .carousel-inner .active.right {
+			opacity: 0;
+		}
+
+		.carousel-fade .carousel-inner .active,
+		.carousel-fade .carousel-inner .next.left,
+		.carousel-fade .carousel-inner .prev.right {
+			opacity: 1;
+		}
+
+		.carousel-fade .carousel-inner .next,
+		.carousel-fade .carousel-inner .prev,
+		.carousel-fade .carousel-inner .active.left,
+		.carousel-fade .carousel-inner .active.right {
+			left: 0;
+			-webkit-transform: translate3d(0, 0, 0);
+			transform: translate3d(0, 0, 0);
+		}
+
+		.carousel-fade .carousel-control {
+			z-index: 2;
+		}
+
+		.carousel-control {
+			background-image: none !important;
+		}
+
+		/* Carousel Controls: */
+
+		svg.right {
+			top: 50%;
+			right: 30px;
+			position: absolute;
+			margin-top: -10px !important;
+		}
+
+		svg.left {
+			top: 50%;
+			left: 30px;
+			position: absolute;
+			margin-top: -10px !important;
+		}
 
 	</style>
 @endsection
 
 @section('main')
 
-	<div id="carousel-main" class="carousel slide animated fadeInDown" data-ride="carousel">
+	<div id="carousel-main" class="carousel slide carousel-fade animated fadeInDown" data-ride="carousel">
 		<ol class="carousel-indicators">
 			<li data-target="#carousel-main" data-slide-to="0" class="active"></li>
 			<li data-target="#carousel-main" data-slide-to="1"></li>
@@ -29,12 +78,10 @@
 			</div>
 		</div>
 		<a class="left carousel-control" href="#carousel-main" role="button" data-slide="prev">
-			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>
+			<i class="fas fa-angle-left left"></i>
 		</a>
 		<a class="right carousel-control" href="#carousel-main" role="button" data-slide="next">
-			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-			<span class="sr-only">Next</span>
+			<i class="fas fa-angle-right right"></i>
 		</a>
 	</div>
 
