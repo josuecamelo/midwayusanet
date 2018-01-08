adb592d5a008c65524488f305dc5d52806e17dba
Futura One <futuraone@icloud.com>
2018-01-08 15:37:18 -0200

    custom plan form

--- resources/views/site/custom-plan.blade.php
+++ resources/views/site/custom-plan.blade.php
@@ -24,6 +24,10 @@
 			text-align: center;
 			margin-bottom: 25px;
 		}
+
+		.form {
+			text-align: center;
+		}
 	</style>
 @endsection
 
@@ -92,6 +96,19 @@
 					</a>
 				</div>
 			</div>
+		</div>
+
+		<div class="form">
+			<h2>Lose Weight</h2>
+			<img src="{{ asset('img/custom-plan/build-muscle-female-form.jpg') }}" class="img-rounded" alt="">
+			<h4>Please enter your name and email</h4>
+			<p>Your customized fitness plan will be sent to the email address you enter below free of charge.
+				<br>We here at Midway Labs USA are committed to helping you achieve your fitness goals and are here to help you every step of the way!</br>
+			<form class="form-inline" method="post" action="/">
+				<input type="text" name="name" class="form-control" placeholder="Full Name">
+				<input type="email" name="email" class="form-control" placeholder="Email">
+				<button class="btn btn-primary">Send me my free plan</button>
+			</form>
 		</div>
 
 	</div>
