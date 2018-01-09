ba6ece40887603bc01beb21dd18959ae26112486
Futura One <futuraone@icloud.com>
2018-01-09 13:56:56 -0200

    files email mail custom plan

--- app/Mail/CustomPlanEmail.php
+++ app/Mail/CustomPlanEmail.php
@@ -9,26 +9,34 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 
 class CustomPlanEmail extends Mailable
 {
-    use Queueable, SerializesModels;
-    public $data;
-
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct($data)
-    {
-        $this->data = $data;
-    }
-
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
-    {
-	    return $this->subject('Team Midway')->view('site.emails.custom-plan');
-    }
+	use Queueable, SerializesModels;
+	public $data;
+	
+	/**
+	 * Create a new message instance.
+	 *
+	 * @return void
+	 */
+	public function __construct($data)
+	{
+		$this->data = $data;
+	}
+	
+	/**
+	 * Build the message.
+	 *
+	 * @return $this
+	 */
+	public function build()
+	{
+		$goal = kebab_case($this->data->goal);
+		$gender = $this->data->gender;
+		
+		return $this
+			->subject('Team Midway')
+			->view('site.emails.custom-plan')
+			->attach('pdf/custom-plan/' . $gender . '/' . $goal . '.pdf', [
+				'mime' => 'application/pdf',
+			]);
+	}
 }
