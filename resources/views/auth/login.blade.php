31b9ef62238b7bd70f3e95cc9a570ea9d09be513
Futura One <futuraone@icloud.com>
2018-01-11 08:11:55 -0200

    translate login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
 							{{ csrf_field() }}
 
 							<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-								<label for="email" class="col-md-4 control-label">E-Mail</label>
+								<label for="email" class="col-md-4 control-label">Email</label>
 
 								<div class="col-md-6">
 									<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -27,7 +27,7 @@
 							</div>
 
 							<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-								<label for="password" class="col-md-4 control-label">Senha</label>
+								<label for="password" class="col-md-4 control-label">Password</label>
 
 								<div class="col-md-6">
 									<input id="password" type="password" class="form-control" name="password" required>
@@ -44,7 +44,7 @@
 								<div class="col-md-6 col-md-offset-4">
 									<div class="checkbox">
 										<label>
-											<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Lembre-me
+											<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember me
 										</label>
 									</div>
 								</div>
@@ -53,11 +53,11 @@
 							<div class="form-group">
 								<div class="col-md-8 col-md-offset-4">
 									<button type="submit" class="btn btn-primary">
-										Entrar
+										Enter
 									</button>
 
 									<a class="btn btn-link" href="{{ route('password.request') }}">
-										Esqueceu sua senha?
+										Forgot your password?
 									</a>
 								</div>
 							</div>
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Resetar Senha</div>
+                <div class="panel-heading">Reset Password</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -18,7 +18,7 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail</label>
+                            <label for="email" class="col-md-4 control-label">Email</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -34,7 +34,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Enviar link para resetar senha
+                                    Send link to reset password
                                 </button>
                                 <a class="btn btn-link" href="{{ route('login') }}">
                                     Login
