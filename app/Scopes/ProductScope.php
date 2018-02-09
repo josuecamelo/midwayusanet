cd26bf6fff3a928b9247d240f411cae163bd9c17
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2018-02-09 09:50:42 -0200

    Scope to Products

---
+++ app/Scopes/ProductScope.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: josuecamelo
+ * Date: 09/02/18
+ * Time: 09:34
+ */
+
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class ProductScope implements Scope
+{
+
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder $builder
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        return $builder->where('visibility', 1);
+    }
+}
