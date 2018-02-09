bb85c73e6271e9e2e0a7b6618447c4c2c616eceb
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2018-02-09 10:43:05 -0200

    Scope to Products Tests

--- app/Scopes/ProductScope.php
+++ app/Scopes/ProductScope.php
@@ -13,6 +13,7 @@ namespace App\Scopes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\Auth;
 
 class ProductScope implements Scope
 {
