d6d436b3f43ada4e952e6d201bde511cf30f9674
Futura One <futuraone@icloud.com>
2018-03-16 09:49:19 -0300

    about

--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -54,7 +54,7 @@
 		<div class="text">
 			<p>This is our mission, to supplement your journey into building your best version of yourself! With this concept in mind, Midway Labs USA was founded in 1991 by visionary entrepreneur Wilton Colle, bringing the science and technology of the dietary supplement industry to millions of consumers. With extensive international presence and over 25 years in the market, Midway Labs USA is today one of the largest supplement brands in the world. All products are proudly manufactured in the United States of America in GMP and FDA registered facilities, supervised by a team of renowned professional scientists, doctors and nutritionists.</p>
 			{{--<p><img src="{{ asset('img/about-usa.jpg') }}" alt="" class="img-res"></p>--}}
-			<p>Currently, Midway Labs USA which is on the board of directors of the American Chamber of Washington DC. We are also a proud supporter and sponsor of several events, sports and athletes, such as the world famous soccer player Kaká, The UFC Champions Nogueira Brothers, the IFBB pro Felipe Franco, The first ever Women’s Physique Karina Nascimento, Latin America MovieStars Giovanna Ewbank & Bruno Gagliasso as well as being the Diamond Sponsor at the Arnold Classic USA and Brazil, Title Sponsor at all 4 Europa Games, Official Sponsor of IFBB (International Federation of Bodybuilding), Official Sponsor of the Boston City Soccer Club, StockCar Racing, Athina Onassis Longiness and several other athletes, teams and events worldwide.</p>
+			<p>Midway Labs USA  is currently on the board of directors of the American Chamber of Washington, DC. We are also a proud supporter and sponsor of several events, sports and athletes, such as the world famous soccer player Kaká, the UFC Champions Nogueira Brothers, the IFBB pro Felipe Franco, the first ever Women’s Physique Karina Nascimento, Athina Onassis Longiness, and Latin America movie stars Giovanna Ewbank and Bruno Gagliasso We are also the Main Sponsor  at the Arnold Classic USA and Brazil, Title Sponsor at all 4 Europa Games, Official Sponsor of IFBB (International Federation of Bodybuilding), and Official Sponsor of the Boston City Soccer Club, StockCar Racing, and other teams and events worldwide.</p>
 			{{--<p><img src="{{ asset('img/about-usa-2.jpg') }}" alt="" class="img-res"></p>--}}
 		</div>
 	</div>
@@ -67,7 +67,7 @@
 	</div>
 
 	<div id="packshot">
-		<p class="container">With over 500 SKUS, selling inside the main big box retailers, advertising in the main magazines and nationwide TV commercials in South America, Midway Labs USA is ever expanding globally with innovative and unique product lines such as Glamour Nutrition (Nutri-Cosmetics) and Military Trail (Premium Supplements).</p>
+		<p class="container">With over 500 products selling at the major big box retailers, advertising in major magazines and airing nTV commercials across  South America, Midway Labs USA is ever expanding globally with innovative and unique product lines such as Glamour Nutrition (Nutri-Cosmetics) and Military Trail (Premium Supplements).</p>
 		<p><img src="{{ asset('img/packshot-usa.png') }}" alt="" class="img-res"></p>
 	</div>
 
@@ -76,13 +76,15 @@
 			<div class="col-md-6">
 				<img src="{{ asset('img/banner-glamour.png') }}" alt="" class="img-responsive img-rounded">
 				<h2>BEAUTY COMES FROM WITHIN</h2>
-				<p>Glamour Nutrition was created with this belief in mind and it’s mission is to empower women worldwide from the inside out! Created by women for women, Catherine Colle together with the scientific council from Midway Labs USA, developed a specific line of beauty from within products based on scientific research and proven results. A true Nutri-Cosmetic line of products designed to bring out your natural beauty using the finest ingredients and technology. Because Beauty, Love and Confidence all starts from within.</p>
+				<p>Glamour Nutrition was created with this belief in mind and it’s mission is to empower women worldwide from the inside out! Created by women for women, Catherine Colle, together with the scientific council from Midway Labs USA, developed a specific line of beauty from within products based on scientific research and proven results. Glamour Nutrition is a true Nutri-Cosmetic line of products designed to bring out your natural beauty using the finest ingredients and technology. Because Beauty, Love and Confidence all starts from within.</p>
 			</div>
 			<div class="col-md-6">
 				<img src="{{ asset('img/banner-military.png') }}" alt="" class="img-responsive img-rounded">
 				<h2>MILITARY TRAIL</h2>
-				<p>Military Trail is the line of products created to honor those who always have to be physically prepared to defend us. While risking their lives daily they must always be ready for any challenge.</p>
-				<p>In honor of the brave men and women who are active-duty or veterans of the U.S. Military and law enforcement. 5% of u.s. gross sales proceeds for military trail will be donated to charities that support our active service members, veterans, law enforcement and their families.*</p>
+				<p>Military Trail is the line of products created to honor those who always have to be physically prepared to defend us. While risking their lives daily, members of the military must be constantly ready for any challenge.</p>
+				<p>In honor of the brave men and women who are active duty or veterans of the U.S. Military and law enforcement. 5% of Military Trail’s U.S. gross sales will be donated to charities that support our active service members, veterans, law enforcement and their families.*</p>
+				<p>All Midway Labs USA products are manufactured in GMP (Good Manufacturing Practices) and FDA registered facilities under rigorous quality control and supervised by a dynamic and experienced team of scientists, formulators and product managers. As a result our products are recognized as one of the top dietary supplement brands worldwide.</p>
+				<p><a href="{{ route('team-midway') }}">Team Midway</a></p>
 			</div>
 		</div>
 		<div class="row">
