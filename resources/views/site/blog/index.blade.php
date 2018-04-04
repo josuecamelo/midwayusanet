6e343cf5e040bce9892b97a982cfb970eeacb40b
Futura One <futuraone@icloud.com>
2018-04-04 11:45:02 -0300

    conselho cientifico blog

--- resources/views/site/blog/index.blade.php
+++ resources/views/site/blog/index.blade.php
@@ -36,6 +36,33 @@
 @section('main')
 	<div class="container">
 		<h1>{{$title}}</h1>
+		@if($t==2)
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="text-center">HIGH QUALITY CONTROL & RESEARCH</h2>
+					<p>All of Midway Labs USA products are manufactured in GMP (Good Manufacturing Practices) and FDA registered facilities. Under rigorous quality control and supervised by a dynamic and experienced team of scientists, formulators and product managers. As a result our products are recognized as one of the top dietary supplement brands worldwide.</p>
+				</div>
+			</div>
+			<p class="text-center"><b>OUR SCIENTIFIC COUNCIL</b></p>
+			<div class="row text-center">
+				<div class="col-md-3">
+					<img src="{{ asset('img/gerseli.png') }}" alt="" class="img-rounded">
+					<p>Dr. Gerseli Angeli</p>
+				</div>
+				<div class="col-md-3">
+					<img src="{{ asset('img/turibio.png') }}" alt="" class="img-rounded">
+					<p>Dr. Turíbio Leite de Barros</p>
+				</div>
+				<div class="col-md-3">
+					<img src="{{ asset('img/dirceu.png') }}" alt="" class="img-rounded">
+					<p>Dr. Dirceu Raposo</p>
+				</div>
+				<div class="col-md-3">
+					<img src="{{ asset('img/karen.png') }}" alt="" class="img-rounded">
+					<p>Karen Lima</p>
+				</div>
+			</div>
+		@endif
 		@foreach($posts->chunk(3) as $post)
 			<div class="row linha">
 				@foreach($post as $p)
