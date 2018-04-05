423c3d2c65deb4078c71751433798403489890ac
Futura One <futuraone@icloud.com>
2018-04-05 14:31:21 -0300

    quality

--- resources/views/site/quality.blade.php
+++ resources/views/site/quality.blade.php
@@ -2,21 +2,22 @@
 
 @section('main')
 
-<section>
+	<section>
 
-	<header>
+		<header>
+			<div class="container">
+				<h1 class="h1">Our Commitment to Quality</h1>
+			</div>
+		</header>
+
 		<div class="container">
-			<h1 class="h1">Quality</h1>
-		</div>
-	</header>
-
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<p class="text-center">Comming Soon</p>
+			<div class="row">
+				<div class="col-md-12">
+					<p>Midway Labs USA, with its excellent reputation as a leader in the international supplements industry, is dedicated to producing the highest quality supplements on the market. All Midway Lab USA products are proudly manufactured in the United States of America using Good Manufacturing Practices (GMP) in FDA registered facilities.</p>
+					<p>Each product is manufactured under rigorous quality control while being supervised by a dynamic and experienced team of scientists, chemists, nutritionists, and product managers. As a result of our dedication to quality and attention to detail, Midway Labs USA is recognized as one of the top supplement brands in the world.</p>
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
 
 @endsection
