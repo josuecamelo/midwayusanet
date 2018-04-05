ed0011e35a35f1f38d05c7f5fc9c774f3c24b17b
Futura One <futuraone@icloud.com>
2018-04-05 14:38:08 -0300

    conselho cientifico

--- resources/views/site/quality.blade.php
+++ resources/views/site/quality.blade.php
@@ -1,8 +1,24 @@
 @extends('site.main')
 
+@section('css')
+	<style>
+		#quality h2 {
+			margin-top: 40px;
+		}
+
+		#quality h3 {
+			font-size: 20px;
+		}
+
+		#quality p {
+			text-align: justify;
+		}
+	</style>
+@endsection
+
 @section('main')
 
-	<section>
+	<section id="quality">
 
 		<header>
 			<div class="container">
@@ -18,6 +34,24 @@
 					<h2>Midway Labs USA’s Scientific Counsel</h2>
 					<p>Our team of advisors includes several highly talented and dedicated scientists, chemists, medical doctors, and nutritionists, all of whom are dedicated to helping Midway Labs USA create the best possible products. Each expert on our counsel is involved in every step of the production cycle from research and development to manufacturing and testing. The counsel conducts extensive research to ensure that not only do our products live up to the claims made about each product, but also that the products are safe to take and are made from the highest-quality of ingredients available.</p>
 					<h3>Our Scientific Counsel Includes:</h3>
+					<div class="row">
+						<div class="col-md-3">
+							<h4>Dr. Gerseli Angeli</h4>
+							<img src="{{ asset('img/gerseli.png') }}" alt="" class="img-rounded">
+						</div>
+						<div class="col-md-3">
+							<h4>Dr. Turíbio Leite de Barros</h4>
+							<img src="{{ asset('img/turibio.png') }}" alt="" class="img-rounded">
+						</div>
+						<div class="col-md-3">
+							<h4>Dr. Dirceu Raposo</h4>
+							<img src="{{ asset('img/dirceu.png') }}" alt="" class="img-rounded">
+						</div>
+						<div class="col-md-3">
+							<h4>Karen Lima</h4>
+							<img src="{{ asset('img/karen.png') }}" alt="" class="img-rounded">
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
