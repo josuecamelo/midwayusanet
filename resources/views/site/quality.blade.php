718d884ac9cd5c6567afbfc224ffdc4757d63c0a
Futura One <futuraone@icloud.com>
2018-04-05 14:44:27 -0300

    centr img

--- resources/views/site/quality.blade.php
+++ resources/views/site/quality.blade.php
@@ -20,6 +20,10 @@
 		#quality p {
 			text-align: justify;
 		}
+
+		.pimg {
+			text-align: center !important;
+		}
 	</style>
 @endsection
 
@@ -44,23 +48,23 @@
 					<div class="row">
 						<div class="col-md-3">
 							<h4>Dr. Gerseli Angeli</h4>
-							<p class="text-center"><img src="{{ asset('img/gerseli.png') }}" alt="" class="img-rounded"></p>
+							<p class="pimg"><img src="{{ asset('img/gerseli.png') }}" alt="" class="img-rounded"></p>
 							<p>Dr. Gerseli Angeli holds a PhD in Sports Science, a master and doctorate in Rehabilitation, and an undergraduate degree in Physiotherapy. She was the physiologist for the Gremio and Barueri Soccer Clubs, as well as the first woman to hold a post in a professional soccer club in Brazil. Dr. Angeli is currently a member of the American College of Sports Medicine, and was previously a scientific director at CEFAME and director at the Instituto de Fisiologia do Exercício (Institute for Exercise Physiology).</p>
 						</div>
 						<div class="col-md-3">
 							<h4>Dr. Turíbio Leite de Barros</h4>
-							<p class="text-center"><img src="{{ asset('img/turibio.png') }}" alt="" class="img-rounded"></p>
+							<p class="pimg"><img src="{{ asset('img/turibio.png') }}" alt="" class="img-rounded"></p>
 							<p>With over 35 years of experience in the supplements industry, Dr. Turibio Leite de Barros not only holds a PhD in Exercise Physiology, he is also considered an authority on the subject. He is a member of the American College of Sports Medicine, and was a professor and coordinator for the Specialization Course in Sports Medicine at UNIFESP. Dr. de Barros was also a physiologist for the São Paulo Soccer Club, and coordinator in the Department of Physiology at Esporte Clube Pinheiros (Pinheiros Sports Club).</p>
 							<p>Dr. de Barros has served as a columnist for Jornal da Tarde for over five years and has published over 100 scientific articles in both national and international sports academic journals. He has published seven books and won the Jabuti Award for Scientific Literature for the publication “O Exercício - aspectos especiais e preventivos” (“Exercise - Special and Preventive Aspects").</p>
 						</div>
 						<div class="col-md-3">
 							<h4>Dr. Dirceu Raposo</h4>
-							<p class="text-center"><img src="{{ asset('img/dirceu.png') }}" alt="" class="img-rounded"></p>
+							<p class="pimg"><img src="{{ asset('img/dirceu.png') }}" alt="" class="img-rounded"></p>
 							<p>Dr. Dirceu Raposo graduated with a degree in Pharmacy and Biochemistry from the University of São Paulo, earned his master degree in Health Sciences from the University of Guarulhos, and his doctorate degree in Pharmaceutical Sciences from Julio de Mesquita Filho São Paulo State University. He specialized in Bioethics at the University of Brasilia, Hospital Administration at São Camilo University, and Homeopathy at the François Lamasson Institute. Dr. Dirceu Raposo also served as the CEO of the Agência Nacional de Vigilância Sanitária (National Health Surveillance Agency).</p>
 						</div>
 						<div class="col-md-3">
 							<h4>Karen Lima</h4>
-							<p class="text-center"><img src="{{ asset('img/karen.png') }}" alt="" class="img-rounded"></p>
+							<p class="pimg"><img src="{{ asset('img/karen.png') }}" alt="" class="img-rounded"></p>
 							<p>Karen Lima specializes in Clinical Sports Nutrition. She has an undergraduate degree in Nutrition and earned her MBA with emphasis on Quality Management and Productivity.</p>
 						</div>
 					</div>
