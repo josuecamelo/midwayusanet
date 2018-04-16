7e88834afc0d9a8661b314cd4861cf1a75cb2cd1
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2018-04-16 10:07:38 -0300

    Correção Solicitada pelo Weliton - Painel Adm
    Erro causando por chamada errada de metodo

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -96,7 +96,7 @@ class ProductController extends Controller
 		//$flavors = $this->productModel->related($product->slug, $product->last_name_slug);
         //dd($this->productModel->ofProduct($product->slug, $product->last_name_slug));
         $flavors = [
-            new \stdClass()
+            new Product()
         ];
 
 		return view('site.supplement', compact(
